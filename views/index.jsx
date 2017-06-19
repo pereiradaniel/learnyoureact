eb7fbd04d2c9a7a544e4509efb3871dbfde4fbd3
Daniel Pereira <pereiradpg@yahoo.ca>
2017-06-19 13:01:33 -0400

    Fix spacing on lines in index.jsx

--- views/index.jsx
+++ views/index.jsx
@@ -71,10 +71,10 @@ class Todo extends React.Component {
   constructor(props) {
       super(props);
       this.state = {checked: false};
-}
+  }
   handleChange(e) {
     this.setState({checked: e.target.checked});
-}
+  }
   render() {
     return (
       <tr>
@@ -89,6 +89,7 @@ class Todo extends React.Component {
     );
   }
 }
+
 Todo.propTypes = {
   title: React.PropTypes.number.isRequired
 };
